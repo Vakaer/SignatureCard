e0b889a4dc70f5d3268ba3362588ce70d9298104
Vakaer <waqarmujeeb153@gmail.com>
2023-04-20 22:42:34 +0500

    Card cplor selection page changing logo and borders

--- src/pages/CustomizeCardPage/CardColorSelectionPage.tsx
+++ src/pages/CustomizeCardPage/CardColorSelectionPage.tsx
@@ -5,19 +5,23 @@ import BorderOne from "../../assets/cards/CardColorSelectionPage/Borders/border-
 import LogoForPinkcard from "../../assets/cards/CardColorSelectionPage/CardLogo/logo-card-1.png";
 import bgBlack from "../../assets/cards/CardColorSelectionPage/background/bg-shiny-black.png";
 import "../CustomizeCardPage/CardColorSelectionPage.css";
-import { CardBgImageData, CardColorsData, CardlogoData } from "./data/data";
+import { CardBgImageData, CardBorderData, CardColorsData, CardlogoData } from "./data/data";
 
 export const CardColorSelection = () => {
 	const [Index, setIndex] = useState(0);
+	const [chip, setChip] = useState(chiplight);
 	const [cardBackground, setCardBackground] = useState(CardBgImageData);
 	const [bg, setBg] = useState<any>([]);
-	const [border, setBorder] = useState(BorderOne);
-	const [chip, setChip] = useState(chiplight);
-	const [cardLogo, setCardLogo] = useState();
+	const [border, setBorder] = useState<any>(BorderOne);
+	const [cardLogo, setCardLogo] = useState<any>(LogoForPinkcard);
 	const [showCardLogo, setShowCardLogo] = useState(false);
+	const [showCardBorder, setShowCardBorder] = useState(false);
 
 	const handleCustomDesign = () => {
 		setShowCardLogo(!showCardLogo);
+	};
+	const handleCustomBorder = () => {
+		setShowCardBorder(!showCardBorder);
 	};
 	useEffect(() => {
 		const newvalue = cardBackground.filter((value, index) => {
@@ -35,28 +39,37 @@ export const CardColorSelection = () => {
 		setBg(newvalue);
 		console.log(back);
 	}, [Index]);
-	// console.log("bg: ", bg);
+
+	const selectCardlogo = (id: number) => {
+		const logo = CardlogoData.find((item) => item.id === id);
+		setCardLogo(logo);
+	};
+	const selectCardBorder = (id: number) => {
+		const border = CardBorderData.find((item) => item.id === id);
+		setBorder(border);
+		console.log(border);
+	};
 	return (
 		<>
-			<section className='customize-card-page-bg d-flex flex-column justify-content-center '>
+			<section className='customize-card-page-bg d-flex flex-column gap-5 justify-content-end '>
+				<div className='back-gif'></div>
 				<div className='col-12 position-relative d-flex'>
-					<div className='back-gif'></div>
 					<div
-						className='d-flex col-lg-10 m-auto align-items-end  justify-content-center flex-row'
+						className='d-flex col-lg-10 m-auto align-items-end  justify-content-between flex-row'
 						style={{ zIndex: "2" }}
 					>
-						<div className='col-lg-2 d-flex flex-column justify-content-center gap-3'>
+						<div className='col-lg-2 d-flex flex-column gap-3 align-items-center justify-content-center'>
 							{showCardLogo && (
 								<div
 									style={{
-										width: "150px",
-										height: "350px",
+										width: "90%",
+										height: "424px",
 										overflowY: "scroll",
-										mixBlendMode: "soft-light",
-										backgroundColor: "#fff",
+										background: "rgba(255, 255, 255, 0.05)",
+										backdropFilter: "blur(6px)",
 										borderRadius: "20px",
 									}}
-									className='d-flex flex-column gap-1'
+									className='d-flex flex-column'
 								>
 									{CardlogoData.map((value: any, index: number) => {
 										const { id, logo } = value;
@@ -64,31 +77,29 @@ export const CardColorSelection = () => {
 											<div
 												key={id}
 												style={{
-													width: "130px",
-													height: "80px",
 													margin: "auto",
 													borderRadius: "20px",
+													padding: "0.2rem 0.5rem 0rem 0.5rem",
 												}}
-												className='pt-2'
 											>
 												<img
 													src={logo}
-													style={{ width: "100%", height: "100%", mixBlendMode: "plus-lighter" }}
-													className='p-1'
+													style={{ width: "100%", height: "100%" }}
+													onClick={() => selectCardlogo(id)}
 												/>
 											</div>
 										);
 									})}
 								</div>
 							)}
-							<div>
+							<div className='d-flex justify-content-center'>
 								<button className='selection-btn' onClick={handleCustomDesign}>
 									Custom Design
 								</button>
 							</div>
 						</div>
 
-						<div className='col-lg-8 d-flex flex-column gap-5 '>
+						<div className='col-lg-6 d-flex flex-column gap-5 '>
 							<div
 								className='d-flex flex-row gap-3 m-auto'
 								style={{ overflow: "hidden", width: "86%" }}
@@ -99,29 +110,26 @@ export const CardColorSelection = () => {
 									return (
 										<div className='position-relative card'>
 											<img
-												src={BorderOne}
+												src={border.border}
 												className='position-absolute'
-												style={{ top: "2rem", left: "2rem", height: "86%" }}
+												style={{ top: "1.5rem", left: "1.5rem", height: "86%" }}
 											/>
-											<img
-												src={cardLogo}
-												className='position-absolute'
-												style={{
-													top: "2rem",
-													left: "2rem",
-													height: "86%",
-													mixBlendMode: "screen",
-												}}
-											/>
-											<img
-												src={chip}
-												className='position-absolute'
-												style={{ top: "6rem", left: "5rem" }}
-											/>
+											<div
+												className='position-absolute d-flex justify-content-center'
+												style={{ mixBlendMode: "screen" }}
+											>
+												<img src={cardLogo.logo} style={{ width: "100%" }} />
+											</div>
+											<div
+												className='position-absolute d-flex justify-content-center '
+												style={{ top: "5rem" }}
+											>
+												<img src={chip} style={{ width: "80%" }} />
+											</div>
 											<img
 												key={Index}
 												src={background}
-												style={{ width: "758px", height: "428px", margin: "auto" }}
+												style={{ width: "100%", height: "100%", margin: "auto" }}
 											/>
 										</div>
 									);
@@ -145,10 +153,54 @@ export const CardColorSelection = () => {
 							</div>
 						</div>
 
-						<div className='col-lg-2 d-flex justify-content-center'>
-							<button className='selection-btn'>Selection of borders</button>
+						<div className='col-lg-2 justify-content-center'>
+							<div className=' d-flex flex-column gap-3 align-items-center justify-content-center'>
+								{showCardBorder && (
+									<div
+										style={{
+											width: "90%",
+											height: "300px",
+											overflowY: "scroll",
+											background: "rgba(255, 255, 255, 0.05)",
+											backdropFilter: "blur(6px)",
+											borderRadius: "20px",
+										}}
+										className='d-flex flex-column gap-1'
+									>
+										{CardBorderData.map((value: any, index: number) => {
+											const { id, border } = value;
+											return (
+												<div
+													key={id}
+													style={{
+														margin: "auto",
+														borderRadius: "20px",
+														padding: "0.2rem 0.5rem 0rem 0.5rem",
+													}}
+													className='pt-2'
+												>
+													<img
+														src={border}
+														style={{ width: "100%", height: "100%", mixBlendMode: "plus-lighter" }}
+														className='p-1'
+														onClick={() => selectCardBorder(id)}
+													/>
+												</div>
+											);
+										})}
+									</div>
+								)}
+								<div className='d-flex justify-content-center'>
+									<button className='selection-btn' onClick={handleCustomBorder}>
+										Selection of borders
+									</button>
+								</div>
+							</div>
 						</div>
 					</div>
+				</div>
+				<div className='d-flex justify-content-center'>
+					<button className='selection-btn'>Order now</button>
 				</div>
 			</section>
 		</>
