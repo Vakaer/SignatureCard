1f1dc1810b651dfcfa2a86959c0211ae0c78f4d7
Vakaer <waqarmujeeb153@gmail.com>
2023-04-28 21:40:55 +0500

    menu button icon changed

--- src/layouts/NavbarMenuButton.tsx
+++ src/layouts/NavbarMenuButton.tsx
@@ -1,19 +1,18 @@
 import React, { useState, MouseEvent } from "react";
 import "../components/Navbar.css";
+import "./NavbarMenuButton.css"
+import { RxHamburgerMenu } from 'react-icons/rx'
 import icHamburger from "../assets/icons/ic-hamburger.svg";
 export type MenuProps = {
 	//isOpen: boolean;
-	onClick: (event: React.MouseEvent<HTMLImageElement>) => void;
+	onClick: (event: React.MouseEvent<HTMLDivElement>) => void;
 };
 const NavbarMenuButton = (props: MenuProps) => {
 	const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 	return (
-		<img
-			onClick={(event) => props.onClick(event)}
-			src={icHamburger}
-			className='text-center icons'
-			alt='Signature Logo'
-		/>
+		<div className=" hamburger-menu btn btn-light rounded-circle" onClick={(event) => props.onClick(event)}>
+			<RxHamburgerMenu className="hamburger-svg" />
+		</div>
 	);
 };
 export default NavbarMenuButton;
